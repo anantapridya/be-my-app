be044a0c1b3608edd49db1bc71de9f3c94d6872e
ananta20pridya <ananta20pridya@gmail.com>
2024-11-03 17:06:27 +0700

    config deploy

--- server.js
+++ server.js
@@ -34,10 +34,7 @@ app.use("/api/user", userSessionRoutes);
 app.use("/api/message", messageRoutes);
 
 db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(db.url)
   .then(() => {
     console.log("Connected to the database!");
   })
@@ -45,6 +42,9 @@ db.mongoose
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
+
+// const mongoose = require("mongoose");
+// const dbConfig = require("./config/db.config");
 
 // Function to connect to the database
 // async function connectDB() {
